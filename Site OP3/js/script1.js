cd2f4adab99531a310dba3c52944c2ac35b3958d
JenniferSchultz <94943478+JenniferSchultz@users.noreply.github.com>
2024-02-13 18:03:20 -0300

    Add files via upload

---
+++ Site OP3/js/script1.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+    var previousScroll = 0;
+
+    $(window).scroll(function () {
+        var currentScroll = $(this).scrollTop();
+        if (currentScroll > previousScroll) {
+            //Direction: Down
+            $(".navbar").fadeOut();
+        } else {
+            //Direction: Up
+            $(".navbar").fadeIn();
+        }
+
+        previousScroll = currentScroll;
+
+        if (currentScroll == 0) {
+            $(".navbar").css("position", "relative");
+        } else {
+            $(".navbar").css("position", "fixed");
+        }
+    });
+});
